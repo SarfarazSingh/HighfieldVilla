7d6f620b9053a5145bb226c9120a9396b738bce7
Sarfaraz Singh <sarfarazsingh@192.168.1.6>
2025-06-09 22:48:48 +0530

    🔥 Force update: Switch to Firebase v8 CDN

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ function addRow(tb, b) {
     <td>${b.checkOut}</td>
     <td>${b.numRooms}</td>
     <td>${b.roomType}</td>
+    <td>${b.advance}</td>
     <td><button class="editBtn">✏️</button></td>
     <td><button class="delBtn">🗑️</button></td>`;
   tb.appendChild(tr);
